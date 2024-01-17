83564fff1f88ebe3847d59ac1ff88ef918d6521c
Thalia <thaliatelopoulou@gmail.com>
2024-01-17 19:10:02 +0200

    setting up the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
