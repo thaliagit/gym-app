9e4222d033bb042e750cf1abc2d882a814f8752e
Thalia <thaliatelopoulou@gmail.com>
2024-01-17 20:02:22 +0200

    navbar styling completed

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
