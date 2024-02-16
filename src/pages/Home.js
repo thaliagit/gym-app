0947d54bbd2181d54480ffa4198afcb998cc60f1
Thalia <thaliatelopoulou@gmail.com>
2024-02-16 20:07:19 +0200

    hero banner layout completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../Components/HeroBanner";
+import SearchExercises from "../Components/SearchExercises";
+import Exercises from "../Components/Exercises";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 }
 
 export default Home;
